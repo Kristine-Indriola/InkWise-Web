3e1be632fe6f6a78c542074197cd8a0630f32dbf
frechy29 <ganzalinof@gmail.com>
2025-11-14 14:17:47 +0800

    newwww

--- ink-wise/postcss.config.js
+++ ink-wise/postcss.config.js
@@ -1,7 +1,7 @@
 // postcss.config.js
 export default {
     plugins: {
-        "@tailwindcss/postcss": {}, // ✅ replace tailwindcss with this
+        tailwindcss: {}, // ✅ use tailwindcss for v3
         autoprefixer: {},
     },
 };
